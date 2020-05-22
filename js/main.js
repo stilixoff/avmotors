614d255bbf7b952f08c5832ba197041f2a5c9edf
Stilixoff <stilixoff@gmail.com>
2020-05-22 22:11:33 +0300

    add img map

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
-    // document.getElementById('map').addEventListener("click", function(){
-    //     document.getElementById("map_box").innerHTML = `<iframe class="map"
-    //     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2647.9282343753353!2d35.01986151565885!3d48.41952207924683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbe3554482fea5%3A0x5293687204501a39!2z0JfQsNC_0L7RgNGW0LfRjNC60LUg0YjQvtGB0LUsIDI20JEsINCU0L3RltC_0YDQvsyBLCDQlNC90ZbQv9GA0L7Qv9C10YLRgNC-0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgNDkwMDA!5e0!3m2!1suk!2sua!4v1559479956614!5m2!1suk!2sua"
-    //     frameborder="0" style="border:0" allowfullscreen></iframe>`
-    // })
+    document.getElementById('map').addEventListener("click", function(){
+        document.getElementById("map").innerHTML = `<iframe class="map_iframe"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2647.9282343753353!2d35.01986151565885!3d48.41952207924683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbe3554482fea5%3A0x5293687204501a39!2z0JfQsNC_0L7RgNGW0LfRjNC60LUg0YjQvtGB0LUsIDI20JEsINCU0L3RltC_0YDQvsyBLCDQlNC90ZbQv9GA0L7Qv9C10YLRgNC-0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgNDkwMDA!5e0!3m2!1suk!2sua!4v1559479956614!5m2!1suk!2sua"
+        frameborder="0" style="border:0" allowfullscreen></iframe>`
+    })
 
     document.addEventListener("DOMContentLoaded", function() {
         var lazyloadImages = document.querySelectorAll("img.lazy");    
